a2e1ca6956038bab665542e8aae079220c954263
Thais Victória <thaisvsilva07@gmail.com>
2026-01-12 00:45:51 -0300

    adiciona karate.log

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -1,5 +1,8 @@
 function fn() {
-    var config = {}
+    var env = karate.env;
+    var config = {env: env}
+
+    karate.log('karate.env system property was:', env);
     config.baseUrl = 'https://bookstore.demoqa.com'
 
     config.username = 'user_' + java.util.UUID.randomUUID()
